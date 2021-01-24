d83f921744f49d1bdbacebc2d536178e08d2862a
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-24 23:36:01 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# uber_clone_firebase
+ 
---
+++ android/app/src/main/kotlin/com/example/uber_clone_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uber_clone_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
